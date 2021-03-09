27b6470fe62fdb64d76a444cde65e0322c9f118e
rizqaalfi <rizqaalfiani7@gmail.com>
2021-03-09 14:03:23 +0700

    rev 1

--- tugas_1/jwb3.php
+++ tugas_1/jwb3.php
@@ -9,7 +9,7 @@ class mahasiswa
   public $prodi;
   public $angkatan;
 
-  public function matkul_web($prodi)
+  public function waktu_berangkat($prodi)
   {
     if ($prodi == "MIF") {
       return "Senin";
@@ -39,7 +39,7 @@ class mahasiswa
 
   public function sidang($angkatan)
   {
-    if ($angkatan < 2018) {
+    if ($angkatan <= 2018) {
       return "ya";
     } else {
       return "tidak";
@@ -48,5 +48,31 @@ class mahasiswa
 }
 
 //object
-$rizqa = new mahasiswa();
-$maya = new mahasiswa();
+$mhs1 = new mahasiswa();
+$mhs2 = new mahasiswa();
+
+//set mhs1
+$mhs1->nama = "Rizqa Alfiani";
+$mhs1->prodi = "MIF";
+$mhs1->tempat_tinggal = "jember";
+$mhs1->nim = "31191919";
+$mhs1->angkatan = "2019";
+
+
+//set mhs2
+$mhs2->nama = "Laila Majnun";
+$mhs2->prodi = "TIF";
+$mhs2->tempat_tinggal = "banyuwangi";
+$mhs2->nim = "31191811";
+$mhs2->angkatan = "2017";
+
+//get and testing
+echo "Nama : " . $mhs1->nama . "<br>" .
+  "Matkul Web : " . $mhs1->waktu_berangkat($mhs1->prodi) . "<br>" .
+  "Tempat PKL : " . $mhs1->PKL($mhs1->tempat_tinggal, $mhs1->nim) . "<br>" .
+  "Sidang tahun ini? " . $mhs1->sidang($mhs1->angkatan) . "<br><br>";
+
+echo "Nama : " . $mhs2->nama . "<br>" .
+  "Matkul Web : " . $mhs2->waktu_berangkat($mhs2->prodi) . "<br>" .
+  "Tempat PKL : " . $mhs2->PKL($mhs2->tempat_tinggal, $mhs2->nim) . "<br>" .
+  "Sidang tahun ini? " . $mhs2->sidang($mhs2->angkatan);
